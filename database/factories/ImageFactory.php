fa1222ddd55cad041ea695b1cf1a6852854d4f9b
Dung Nguyen H. A. [4] <dung.nguyen4@asiantech.vn>
2020-03-12 10:17:59 +0700

    Fix Database 2nd

--- database/factories/ImageFactory.php
+++ database/factories/ImageFactory.php
@@ -2,28 +2,17 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
-use App\Model;
-use App\Image;
-use App\User;
-use App\Product;
 use Faker\Generator as Faker;
+use App\Image;
 
 $factory->define(Image::class, function (Faker $faker) {
-    $imageable_type = $faker->randomElement(['user','product']);
-    $imageable_id = '';
-
-    if ($imageable_type == 'user') 
-    {
-        $imageable_id = User::all()->pluck('id');
-    } elseif ($imageable_type == 'product')
-    {
-        $imageable_id = Product::all()->pluck('id');
-    }
+    $imageable_type = $faker->randomElement([App\Product::class, App\User::class]);
+    $id = $imageable_type::all()->pluck('id');
 
     return [
         'name' => $faker->name,
         'description' => $faker->text,
-        'imageable_id' => $faker->randomElement($imageable_id),
         'imageable_type' => $imageable_type,
+        'imageable_id' => $faker->randomElement($id),
     ];
 });
